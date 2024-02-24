c426d5aa3a2eba3ec20c291ada59dccd1a293249
TheEpicGamer274 <smithlucas464@gmail.com>
2024-02-24 01:10:29 -0500

    Fixes

--- src/main/java/gregtech/api/enums/ItemList.java
+++ src/main/java/gregtech/api/enums/ItemList.java
@@ -1779,6 +1779,7 @@ public enum ItemList implements IItemContainer {
     Machine_Multi_Assemblyline,
     Machine_Multi_DieselEngine,
     Machine_Multi_ExtremeDieselEngine,
+    CircuitAssemblyMulti,
     QuantumEye,
     QuantumStar,
     Gravistar,
