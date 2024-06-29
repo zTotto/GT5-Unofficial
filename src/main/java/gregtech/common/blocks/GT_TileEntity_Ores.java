3ba183b28ebb08d4f5ed71401648bb33bd949fb5
Lorenzo Todisco <49115537+zTotto@users.noreply.github.com>
2024-06-29 09:43:51 +0200

    Made fortune more than 3 work

--- src/main/java/gregtech/common/blocks/GT_TileEntity_Ores.java
+++ src/main/java/gregtech/common/blocks/GT_TileEntity_Ores.java
@@ -334,10 +334,10 @@ public class GT_TileEntity_Ores extends TileEntity implements IAllSidedTexturedT
                         // if not shouldFortune or not isNatural then get normal drops
                         // if not shouldFortune and isNatural then get normal drops
                         // if shouldFortune and not isNatural then get normal drops
-                        if (shouldFortune && this.mNatural && aFortune > 0) {
+                        if (aFortune > 0) { // shouldFortune && this.mNatural && aFortune > 0)
                             int aMinAmount = 1;
                             // Max applicable fortune
-                            if (aFortune > 3) aFortune = 3;
+                            // if (aFortune > 3) aFortune = 3;
                             int amount = aMinAmount
                                 + Math.max(worldObj.rand.nextInt(aFortune * (tIsRich ? 2 : 1) + 2) - 1, 0);
                             for (int i = 0; i < amount; i++) {
